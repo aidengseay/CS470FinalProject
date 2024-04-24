294d4a71295a8dfc8785cd227a44fd957a521015
aidengseay <aidengseay@gmail.com>
2024-04-24 13:54:33 -0700

    added naive bayes algos

--- Utilities/NaiveBayes.py
+++ Utilities/NaiveBayes.py
@@ -1,6 +1,6 @@
 ################################################################################
 '''
-RETRIEVE/SPLIT DATASET FUNCTION UTILITIES
+NAIVE BAYES FUNCTION UTILITIES
 Credit: Aiden Seay - Spring 2024
 
 DESCRIPTION:
@@ -18,17 +18,36 @@ Implement the naive bayes algorithm.
 # MAIN FUNCTION
 
 '''
-Parameter(s):
-Process: Main algorithm for naive bayes.
+Parameter(s): train_fold, validation_fold, X_test, y_test
+Process: Main algorithm for naive bayes. Includes both training and predictions
 Return:
 Function Dependencies:
 '''
-def naive_bayes_main():
+def naive_bayes_main(train_fold, validation_fold):
     pass
 
 
 ################################################################################
 # SUPPORTING FUNCTIONS
+
+'''
+Parameter(s):
+Process:
+Return:
+Function Dependencies:
+'''
+def predict_naive_bayes():
+    pass
+
+
+'''
+Parameter(s):
+Process:
+Return:
+Function Dependencies:
+'''
+def train_naive_bayes():
+    pass
 
 
 ################################################################################
